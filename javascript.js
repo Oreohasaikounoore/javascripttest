2801821379c89b19969a97be38aaef9e1f738099
Oreohasaikounoore <oreohasaikounoore@gmail.com>
2026-06-03 17:36:57 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,35 @@
-// 【未検証】（GASからの送信部分が未完成なため受信テストは未実施）
-self.addEventListener('push', function(event) {
-  let messageData = 'Hello World';
-  
-  if (event.data) {
-    // ブラウザが自動で復号したテキストを取得
-    messageData = event.data.text();
-  }
+self.addEventListener("install", event => {
+console.log("SW install");
+self.skipWaiting();
+});
 
-  const title = 'GAS x GitHub Pages Web Push';
-  const options = {
-    body: messageData,
-    icon: 'https://www.gstatic.com/images/branding/product/2x/apps_script_64dp.png',
-    badge: 'https://www.gstatic.com/images/branding/product/2x/apps_script_64dp.png'
-  };
+self.addEventListener("activate", event => {
+console.log("SW activate");
+});
 
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
+self.addEventListener("push", event => {
+
+```
+let message =
+    "Hello World";
+
+if (event.data) {
+    message =
+        event.data.text();
+}
+
+event.waitUntil(
+    self.registration.showNotification(
+        "GAS x GitHub Pages Web Push",
+        {
+            body: message,
+            icon:
+                "https://www.gstatic.com/images/branding/product/2x/apps_script_64dp.png",
+            badge:
+                "https://www.gstatic.com/images/branding/product/2x/apps_script_64dp.png"
+        }
+    )
+);
+```
+
 });
