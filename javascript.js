7739c1d31063cb9f19bb72008ae3bbbd86c83f54
Oreohasaikounoore <oreohasaikounoore@gmail.com>
2026-06-03 17:45:39 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,30 @@
 self.addEventListener("install", event => {
-console.log("SW install");
-self.skipWaiting();
+  console.log("SW install");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-console.log("SW activate");
+  console.log("SW activate");
 });
 
+// プッシュ通知を受信したときのイベント
 self.addEventListener("push", event => {
+  let message = "Hello World";
 
-```
-let message =
-    "Hello World";
+  // 通知データがある場合は、そのテキストを取得
+  if (event.data) {
+    message = event.data.text();
+  }
 
-if (event.data) {
-    message =
-        event.data.text();
-}
-
-event.waitUntil(
+  // 通知を表示するまでサービスワーカーを待機させる
+  event.waitUntil(
     self.registration.showNotification(
-        "GAS x GitHub Pages Web Push",
-        {
-            body: message,
-            icon:
-                "https://www.gstatic.com/images/branding/product/2x/apps_script_64dp.png",
-            badge:
-                "https://www.gstatic.com/images/branding/product/2x/apps_script_64dp.png"
-        }
+      "GAS x GitHub Pages Web Push",
+      {
+        body: message,
+        icon: "https://www.gstatic.com/images/branding/product/2x/googleg_64dp.png",
+        badge: "https://www.gstatic.com/images/branding/product/2x/googleg_64dp.png"
+      }
     )
-);
-```
-
+  );
 });
